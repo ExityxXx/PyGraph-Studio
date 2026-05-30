aa6abe7e95b9d7e4ddf34c387c773144e9270600
Exityx <gdlasorum@gmail.com>
2026-05-30 15:01:46 +0300

    deleted

--- node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, uid, header, code):
-        self.uid = uid
-        self.header = header
-        self.code = code
-        
-    def __repr__(self):
-        return f"Node(uid={self.uid}, header={self.header}, code={self.code})"
