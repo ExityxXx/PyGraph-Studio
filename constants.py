7f117a2d73ff0a009c331cb08f20ea0ba50ae37d
Exityx <gdlasorum@gmail.com>
2026-05-30 12:32:49 +0300

    feat: add constant values

---
+++ constants.py
@@ -0,0 +1,28 @@
+class StdColors:
+    FLOW_NODE_NORMAL = "#1C7700"
+    FLOW_NODE_ACTIVE = "#FFD000"
+    FLOW_NODE_DRAGGING = "#33FF00"
+    DATA_NODE_NORMAL = "#7E4396"
+    DATA_NODE_ACTIVE = "#448ECA"
+    START_NODE_NORMAL = "#5A6FE4"
+
+PGS_NODE_ITEMS = [
+    "Функция print()",
+    "Передать данные",
+    "Передать переменную",
+    "Изменить переменную",
+    "Создать переменную",
+    "Вызвать функцию",
+    "Условие",
+    "Макрос (настроить)"
+]
+
+PGS_NODE_CLASSES_STR = [
+    "Node",
+    "PrintNode",
+    "TransferDataNode",
+    "GetVariableNode",
+    "SetVariableNode",
+    "NewVariableNode"
+]
+
